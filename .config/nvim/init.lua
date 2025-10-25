0249c18f61ea7ae97eeb1c80468f246269650332
binil <biniloffcl@gmail.com>
2025-10-25 05:05:26 -0700

    Set tabspace width in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,11 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- set tab width to be 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
 
 -- enable system clipboard for nvim
 vim.opt.clipboard = "unnamedplus"
