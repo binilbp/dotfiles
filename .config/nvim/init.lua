e2d041e06824f45dad8158896421cb75132f45cd
binil <biniloffcl@gmail.com>
2025-10-24 23:17:48 -0700

    Add nvim config and plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,16 @@
+-- enable line number and make it relative
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+-- enable system clipboard for nvim
 vim.opt.clipboard = "unnamedplus"
+
+
+-- lazy plugin setup
+require("config.lazy")
+
+
+
+
+
