2f1fd3b18d72e2fb90bc45e6f8fcb9e71991c4a1
binilbp <biniloffcl@gmail.com>
2026-07-01 10:15:30 +0530

    Add starship.toml file; Modify config.fish to have other shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,15 @@ function fish_greeting
     #enter something to display over every terminal launch
 end
 if status is-interactive
-    abbr -a dots 'cd ~/.dotfiles; git status'
+
+    abbr -a dots 'cd ~/.dotfiles; git status'                           
+    abbr -a bat 'batcat --theme "Dracula"'
+    abbr -a update 'sudo apt update; apt list --upgradable | batcat --file-name "Packages to Update" --theme "Dracula"'
+
+    # clearing screen updated to fake clear
     abbr -a c 'printf "\e[H\e[22J"'
     abbr -a clear 'printf "\e[H\e[22J"'
 
     starship init fish | source
+    enable_transience
 end
